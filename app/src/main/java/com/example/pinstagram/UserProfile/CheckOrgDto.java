8d03306a2d03545ed67adbafa014ad3bf1b73350
rashigambhir <rashigambhir25@gmail.com>
2022-02-07 17:59:58 +0530

    git remote add origin https://github.com/rashigambhir25/pinstagram.git git branch -M main git push -u origin main

---
+++ app/src/main/java/com/example/pinstagram/UserProfile/CheckOrgDto.java
@@ -0,0 +1,22 @@
+package com.example.pinstagram.UserProfile;
+
+public class CheckOrgDto {
+    private String name;
+    private boolean type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isType() {
+        return type;
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
+    }
+}
